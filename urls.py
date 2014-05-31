319540ca8b5e3368c19be8dfcac132c6ba49847f
Eric Wohnlich <oolitegroove@gmail.com>
2014-05-30 22:21:52 -0400

    home page

--- urls.py
+++ urls.py
@@ -4,6 +4,8 @@ import settings
 from django.contrib import admin
 admin.autodiscover()
 
+from huntertools import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
@@ -11,5 +13,6 @@ urlpatterns = patterns('',
 
     url(r'^gearitem/', include('gearitem.urls', namespace="gearitem")),
     url(r'^hunter/', include('hunter.urls', namespace="hunter")),
+    url(r'^$', views.Home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
---
+++ views.py
@@ -0,0 +1,8 @@
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.template import RequestContext, loader
+from django.views import generic
+
+def Home(request):
+ return render(request, 'home.html')
