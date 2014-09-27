b0fd5bf004db575e067fd883db2fe863ccadcd4d
Eric Wohnlich <oolitegroove@gmail.com>
2014-09-27 00:26:06 -0400

    dfsfgsdgsd

--- settings.py
+++ settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'calcs',
     'gearitem',
     'hunter',
-    'calcs',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -48,6 +48,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
